92faf4d3fdda6e5abac952811b56b30502f9c674
yinjianbin <937849667@qq.com>
2019-04-16 13:42:46 +0800

    Update slow_date.sh

--- slow_date.sh
+++ slow_date.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-  /usr/local/mysql/bin/mysql  -uzz-fx-com -p1vCEclfJeuoqlkKN -hrds-1fangxin.mysql.rds.aliyuncs.com  -e "show full processlist"|awk '{for (i=1;i<=5;i++) $i=""; print $0}'|grep -v "NULL" | awk '{if ($1>=1) {print $0}}'|grep -v "Time" >> /home/mysql_slow_log_$(date +%F).txt 
+  /usr/local/mysql/bin/mysql  -u -p -h  -e "show full processlist"|awk '{for (i=1;i<=5;i++) $i=""; print $0}'|grep -v "NULL" | awk '{if ($1>=1) {print $0}}'|grep -v "Time" >> /home/mysql_slow_log_$(date +%F).txt 
 #sort /home/slow.log | uniq -u > /home/zz_mysql_slow_log.txt
 sleep 2
 done
